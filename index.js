c5c65ba6a7270f8b0f461106b9fa564761deaa46
An Nuyen <annuyen@gmail.com>
2018-02-16 23:50:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body)
+  } else {
+    return new Handlebars.SafeString("<stron>" + this.body + "</strong>")
+  }
+})
 
 function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
